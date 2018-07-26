f6379e9aff76583cb5f37d3847d2faa8f8427544
patricio tula <ptula@ekumenlabs.com>
2018-07-26 18:09:21 -0300

    Added batteries data from the diagnostic aggregator

--- src/laptop_battery_monitor/scripts/DockDriveActionClient.py
+++ src/laptop_battery_monitor/scripts/DockDriveActionClient.py
@@ -1,39 +1,5 @@
 #!/usr/bin/env python
 
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2012, Yujin Robot
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of the Yujin Robot nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
-# Author: Younghun Ju <yhju@yujinrobot.com> <yhju83@gmail.com>
-
 import roslib; roslib.load_manifest('kobuki_auto_docking')
 import rospy
 
@@ -41,6 +7,7 @@ import actionlib
 from kobuki_msgs.msg import AutoDockingAction, AutoDockingGoal
 from actionlib_msgs.msg import GoalStatus
 from sensor_msgs.msg import BatteryState
+from diagnostic_msgs.msg import DiagnosticArray
 
 def doneCb(status, result):
     if 0:   
@@ -85,14 +52,18 @@ def dock_drive_client():
 
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.percentage)
-    if data.percentage < 100:
+    batteries_names = ['/Power System/Laptop Battery', "/Power System/Battery"]
+    batteries_values = [element.values for element in data.status if element.name in batteries_names]
+    laptop_battery_percentage = filter(lambda x: x.key == "Percentage (%)", batteries_values[0])[0].value
+    kuboki_battery_percentage = filter(lambda x: x.key == "Percent", batteries_values[1])[0].value
+    if kuboki_battery_percentage < 20 or laptop_battery_percentage < 20:
+        print 'go dock yourself'
         dock_drive_client()
 
 if __name__ == '__main__':
     try:
         rospy.init_node('dock_drive_client_py', anonymous=True)
-        rospy.Subscriber("/laptop_charge", BatteryState, callback)
+        rospy.Subscriber("/diagnostics_agg", DiagnosticArray, callback)
         rospy.spin()
     except rospy.ROSInterruptException: 
         print "program interrupted before completion"

487aca959570376e7182d577a62486d96cbacd91
patricio tula <ptula@ekumenlabs.com>
2018-07-27 14:41:09 -0300

    Updated docking routine

--- src/laptop_battery_monitor/scripts/DockDriveActionClient.py
+++ src/laptop_battery_monitor/scripts/DockDriveActionClient.py
@@ -6,64 +6,69 @@ import rospy
 import actionlib
 from kobuki_msgs.msg import AutoDockingAction, AutoDockingGoal
 from actionlib_msgs.msg import GoalStatus
-from sensor_msgs.msg import BatteryState
 from diagnostic_msgs.msg import DiagnosticArray
 
-def doneCb(status, result):
-    if 0:   
-        print ''
-    elif status == GoalStatus.PENDING   : state='PENDING'
-    elif status == GoalStatus.ACTIVE    : state='ACTIVE'
-    elif status == GoalStatus.PREEMPTED : state='PREEMPTED'
-    elif status == GoalStatus.SUCCEEDED : state='SUCCEEDED'
-    elif status == GoalStatus.ABORTED   : state='ABORTED'
-    elif status == GoalStatus.REJECTED  : state='REJECTED'
-    elif status == GoalStatus.PREEMPTING: state='PREEMPTING'
-    elif status == GoalStatus.RECALLING : state='RECALLING'
-    elif status == GoalStatus.RECALLED  : state='RECALLED'
-    elif status == GoalStatus.LOST      : state='LOST'
-    # Print state of action server
-    print 'Result - [ActionServer: ' + state + ']: ' + result.text
 
-def activeCb():
-    if 0:
-        print 'Action server went active.'
+class AutoDocking:
+    BATTERY_THRESHOLD = 50
 
-def feedbackCb(feedback):
-    # Print state of dock_drive module (or node.)
-    print 'Feedback: [DockDrive: ' + feedback.state + ']: ' + feedback.text
+    def __init__(self):
+        self._ros_node = rospy.init_node('dock_drive_client_py', anonymous=True)
+        self._client = actionlib.SimpleActionClient('dock_drive_action', AutoDockingAction)
+        self._diagnostic_agg_sub = rospy.Subscriber("/diagnostics_agg", DiagnosticArray, self._listen_batteries)
+        self._go_docking = False
 
-def dock_drive_client():
-    # add timeout setting
-    client = actionlib.SimpleActionClient('dock_drive_action', AutoDockingAction)
-    while not client.wait_for_server(rospy.Duration(5.0)):
-        if rospy.is_shutdown(): 
-            return
-        print 'Action server is not connected yet. still waiting...'
+    def _doneCb(status, result):
+        print "it is docked"
+        if 0:   
+            print ''
+        elif status == GoalStatus.PENDING   : state='PENDING'
+        elif status == GoalStatus.ACTIVE    : state='ACTIVE'
+        elif status == GoalStatus.PREEMPTED : state='PREEMPTED'
+        elif status == GoalStatus.SUCCEEDED : state='SUCCEEDED'
+        elif status == GoalStatus.ABORTED   : state='ABORTED'
+        elif status == GoalStatus.REJECTED  : state='REJECTED'
+        elif status == GoalStatus.PREEMPTING: state='PREEMPTING'
+        elif status == GoalStatus.RECALLING : state='RECALLING'
+        elif status == GoalStatus.RECALLED  : state='RECALLED'
+        elif status == GoalStatus.LOST      : state='LOST'
+        # Print state of action server
+        print 'Result - [ActionServer: ' + state + ']: ' + result.text
 
-        goal = AutoDockingGoal()
-        client.send_goal(goal, doneCb, activeCb, feedbackCb)
-        print 'Goal: Sent.'
-        rospy.on_shutdown(client.cancel_goal)
-        client.wait_for_result()
+    def _feedbackCb(feedback):
+        print 'Feedback: [DockDrive: ' + feedback.state + ']: ' + feedback.text
 
-        print '    - status:', client.get_goal_status_text()
-    return client.get_result()
+    def _dock_drive_client(self):
+        while not self._client.wait_for_server(rospy.Duration(5.0)):
+            if rospy.is_shutdown(): 
+                return
+            print 'Action server is not connected yet. still waiting...'
+            
+            goal = AutoDockingGoal()
+            self._client.send_goal(goal, done_cb=self._doneCb, feedback_cb=self._feedbackCb)
+            print 'Goal: Sent.'
 
+            self._go_docking = False
+    
+    def _listen_batteries(self, data):
+        batteries_names = ['/Power System/Laptop Battery', "/Power System/Battery"]
+        batteries_values = [element.values for element in data.status if element.name in batteries_names]
+        laptop_battery_percentage = float(filter(lambda x: x.key == "Percentage (%)", batteries_values[0])[0].value)
+        kuboki_battery_percentage = float(filter(lambda x: x.key == "Percent", batteries_values[1])[0].value)
+        
+        if self._go_docking is False:
+            if kuboki_battery_percentage < self.BATTERY_THRESHOLD or laptop_battery_percentage < self.BATTERY_THRESHOLD:
+                print 'docking'
+                self._dock_drive_client()
+                self._go_docking = True
 
-def callback(data):
-    batteries_names = ['/Power System/Laptop Battery', "/Power System/Battery"]
-    batteries_values = [element.values for element in data.status if element.name in batteries_names]
-    laptop_battery_percentage = filter(lambda x: x.key == "Percentage (%)", batteries_values[0])[0].value
-    kuboki_battery_percentage = filter(lambda x: x.key == "Percent", batteries_values[1])[0].value
-    if kuboki_battery_percentage < 20 or laptop_battery_percentage < 20:
-        print 'go dock yourself'
-        dock_drive_client()
+    def run(self):
+        rospy.spin()
+
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('dock_drive_client_py', anonymous=True)
-        rospy.Subscriber("/diagnostics_agg", DiagnosticArray, callback)
-        rospy.spin()
+        autodock = AutoDocking()
+        autodock.run()
     except rospy.ROSInterruptException: 
         print "program interrupted before completion"
